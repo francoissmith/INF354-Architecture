2d282c34e34f221d0631e324d6fa327295921b7b
francoissmith <u19314486@tuks.co.za>
2023-03-29 02:31:48 +0200

    Add project files.

---
+++ Models/CourseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Architecture.Models
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CourseRepository(AppDbContext appDbContext)
+        {
+                _appDbContext = appDbContext;
+        }
+
+        public async Task AddCourseAsync(string name, string description, string duration)
+        {
+            try
+            {
+                var newCourse = new Course
+                {
+                    Name = name,
+                    Description = description,
+                    Duration = duration
+                };
+
+                await _appDbContext.Courses.AddAsync(newCourse);
+                await _appDbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                throw new Exception($"Error adding course: {ex.Message}");
+            }
+        }
+
+
+        public async Task DeleteCourseAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Course> EditCourseAsync(int id, string name, string description, string duration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Course[]> GetAllCourseAsync()
+        {
+            IQueryable<Course> query = _appDbContext.Courses;
+            return await query.ToArrayAsync();
+        }
+    }
+}
---
+++ Models/ICourseRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Architecture.Models
+{
+    public interface ICourseRepository
+    {
+        // Course
+        Task<Course[]> GetAllCourseAsync();
+
+        Task<Course> EditCourseAsync(int id, string name, string description, string duration);
+
+        Task AddCourseAsync(string name, string descriptionm, string duration);
+
+        Task DeleteCourseAsync(int id);
+
+    }
+}
